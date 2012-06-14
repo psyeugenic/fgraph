aae0517d070c17b5953a24f95b9208c0c67330e2
Björn-Egil Dahlberg <egil@erlang.org>
2012-06-14 23:47:55 +0200

    Rebarify

--- Makefile
+++ Makefile
@@ -1,9 +1,5 @@
-
-all: Makefile
-	erlc -oebin -Iinclude src/fgraph.erl
-	erlc -oebin -Iinclude src/fgraph_win.erl
-	erlc -oebin -Iinclude src/percept_win.erl
-	erlc -oebin -Iinclude src/processes.erl
+all: 
+	./rebar compile
 
 clean:
 	rm ebin/*.beam
---
+++ src/provisual_app.erl
@@ -0,0 +1,16 @@
+-module(provisual_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    provisual_sup:start_link().
+
+stop(_State) ->
+    ok.
